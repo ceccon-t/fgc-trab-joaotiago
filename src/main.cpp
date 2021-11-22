368d5311d99ad7e18521a9aa8ae075b4374beb2c
Tiago Ceccon <ceccon.t@hotmail.com>
2021-11-22 12:15:33 -0300

    Implement dettached free cam movement

--- src/main.cpp
+++ src/main.cpp
@@ -67,6 +67,10 @@
 #define MOVEMENT_BEZIER 3
 #define TIME_MOVEMENT_SCALING_FACTOR 0.01f
 
+// For camera
+#define CAMERA_TYPE_LOOKAT 1
+#define CAMERA_TYPE_FREECAM 2
+
 
 // Estrutura que representa um modelo geométrico carregado a partir de um
 // arquivo ".obj". Veja https://en.wikipedia.org/wiki/Wavefront_.obj_file .
@@ -208,9 +212,22 @@ bool g_MiddleMouseButtonPressed = false; // Análogo para botão do meio do mous
 // usuário através do mouse (veja função CursorPosCallback()). A posição
 // efetiva da câmera é calculada dentro da função main(), dentro do loop de
 // renderização.
-float g_CameraTheta = 0.0f; // Ângulo no plano ZX em relação ao eixo Z
-float g_CameraPhi = 0.0f;   // Ângulo em relação ao eixo Y
-float g_CameraDistance = 3.5f; // Distância da câmera para a origem
+float g_CameraTheta_lookAt = 0.0f; // Ângulo no plano ZX em relação ao eixo Z
+float g_CameraPhi_lookAt = 0.0f;   // Ângulo em relação ao eixo Y
+float g_CameraDistance_lookAt = 3.5f; // Distância da câmera para a origem
+// Variables for free cam
+float g_CameraTheta_freeCam = 3.926826f;
+float g_CameraPhi_freeCam = -0.523797f;
+float g_CameraDistance_freeCam = 2.5f;
+// Variables for cam type
+int g_CamType = CAMERA_TYPE_FREECAM;
+
+// Variables to control player movement
+bool g_KeyWPressed = false;
+bool g_KeyAPressed = false;
+bool g_KeySPressed = false;
+bool g_KeyDPressed = false;
+
 
 // Variáveis que controlam rotação do antebraço
 float g_ForearmAngleZ = 0.0f;
@@ -499,6 +516,8 @@ int main(int argc, char* argv[])
 
     // liveObjects.push_back(esferaUnica);
 
+    // Variables to control camera movement
+    glm::vec4 camera_position_c_freeCam  = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "c", centro da câmera
 
 
     // Ficamos em loop, renderizando, até que o usuário feche a janela
@@ -523,26 +542,55 @@ int main(int argc, char* argv[])
         glUseProgram(program_id);
 
         // Computamos a posição da câmera utilizando coordenadas esféricas.  As
-        // variáveis g_CameraDistance, g_CameraPhi, e g_CameraTheta são
+        // variáveis g_CameraDistance_lookAt, g_CameraPhi_lookAt, e g_CameraTheta_lookAt são
         // controladas pelo mouse do usuário. Veja as funções CursorPosCallback()
         // e ScrollCallback().
-        float r = g_CameraDistance;
-        float y = r*sin(g_CameraPhi);
-        float z = r*cos(g_CameraPhi)*cos(g_CameraTheta);
-        float x = r*cos(g_CameraPhi)*sin(g_CameraTheta);
+        // Look-At Cam
+        float r_lookAt = g_CameraDistance_lookAt;
+        float y_lookAt = r_lookAt*sin(g_CameraPhi_lookAt);
+        float z_lookAt = r_lookAt*cos(g_CameraPhi_lookAt)*cos(g_CameraTheta_lookAt);
+        float x_lookAt = r_lookAt*cos(g_CameraPhi_lookAt)*sin(g_CameraTheta_lookAt);
+
+        // Free Cam
+        float r_freeCam = g_CameraDistance_freeCam;
+        float x_freeCam = r_freeCam*cos(g_CameraPhi_freeCam)*sin(g_CameraTheta_freeCam);
+        float y_freeCam = r_freeCam*sin(g_CameraPhi_freeCam);
+        float z_freeCam = r_freeCam*cos(g_CameraPhi_freeCam)*cos(g_CameraTheta_freeCam);
 
         // Abaixo definimos as varáveis que efetivamente definem a câmera virtual.
         // Veja slides 195-227 e 229-234 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
-        glm::vec4 camera_position_c  = glm::vec4(x,y,z,1.0f); // Ponto "c", centro da câmera
         // ALTERED
+        glm::vec4 camera_position_c_lookAt  = glm::vec4(x_lookAt,y_lookAt,z_lookAt,1.0f); // Ponto "c", centro da câmera
         // glm::vec4 camera_lookat_l    = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
         glm::vec4 camera_lookat_l    = glm::vec4(player.pos.x,player.pos.y,player.pos.z,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
-        glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
-        glm::vec4 camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
+        glm::vec4 camera_view_vector_lookAt = camera_lookat_l - camera_position_c_lookAt; // Vetor "view", sentido para onde a câmera está virada
+        glm::vec4 camera_up_vector_lookAt   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
+
+        // Free cam
+        glm::vec4 camera_view_vector_freeCam = glm::vec4(x_freeCam, y_freeCam, z_freeCam, 0.0f);
+        glm::vec4 camera_up_vector_freeCam = glm::vec4(0.0f, 1.0f, 0.0f, 0.0f);
+        glm::vec4 camera_w_vector = (-camera_view_vector_freeCam) / norm(camera_view_vector_freeCam);
+        glm::vec4 up_cross_w = crossproduct(camera_up_vector_freeCam, camera_w_vector);
+        glm::vec4 camera_u_vector = (up_cross_w) / norm(up_cross_w);
+
+        // Apply movement
+        if (g_CamType == CAMERA_TYPE_FREECAM) {
+            if (g_KeyWPressed) camera_position_c_freeCam  += (-camera_w_vector) * 0.1f; // TODO:  add delta time factor here
+            if (g_KeyAPressed) camera_position_c_freeCam  += (-camera_u_vector) * 0.1f; // TODO:  add delta time factor here
+            if (g_KeySPressed) camera_position_c_freeCam  += camera_w_vector * 0.1f; // TODO:  add delta time factor here
+            if (g_KeyDPressed) camera_position_c_freeCam  += camera_u_vector * 0.1f; // TODO:  add delta time factor here
+        }
+
 
         // Computamos a matriz "View" utilizando os parâmetros da câmera para
         // definir o sistema de coordenadas da câmera.  Veja slides 2-14, 184-190 e 236-242 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
-        glm::mat4 view = Matrix_Camera_View(camera_position_c, camera_view_vector, camera_up_vector);
+        glm::mat4 view = Matrix_Camera_View(camera_position_c_lookAt, camera_view_vector_lookAt, camera_up_vector_lookAt);
+        // ALTERED
+        if (g_CamType == CAMERA_TYPE_LOOKAT) {
+            view = Matrix_Camera_View(camera_position_c_lookAt, camera_view_vector_lookAt, camera_up_vector_lookAt);
+        } else {
+            view = Matrix_Camera_View(camera_position_c_freeCam, camera_view_vector_freeCam, camera_up_vector_freeCam);
+        }
 
         // Agora computamos a matriz de Projeção.
         glm::mat4 projection;
@@ -567,8 +615,8 @@ int main(int argc, char* argv[])
             // Para definição dos valores l, r, b, t ("left", "right", "bottom", "top"),
             // PARA PROJEÇÃO ORTOGRÁFICA veja slides 219-224 do documento Aula_09_Projecoes.pdf.
             // Para simular um "zoom" ortográfico, computamos o valor de "t"
-            // utilizando a variável g_CameraDistance.
-            float t = 1.5f*g_CameraDistance/2.5f;
+            // utilizando a variável g_CameraDistance_lookAt.
+            float t = 1.5f*g_CameraDistance_lookAt/2.5f;
             float b = -t;
             float r = t*g_ScreenRatio;
             float l = -r;
@@ -1471,19 +1519,36 @@ void CursorPosCallback(GLFWwindow* window, double xpos, double ypos)
         float dx = xpos - g_LastCursorPosX;
         float dy = ypos - g_LastCursorPosY;
     
-        // Atualizamos parâmetros da câmera com os deslocamentos
-        g_CameraTheta -= 0.01f*dx;
-        g_CameraPhi   += 0.01f*dy;
-    
-        // Em coordenadas esféricas, o ângulo phi deve ficar entre -pi/2 e +pi/2.
-        float phimax = 3.141592f/2;
-        float phimin = -phimax;
-    
-        if (g_CameraPhi > phimax)
-            g_CameraPhi = phimax;
-    
-        if (g_CameraPhi < phimin)
-            g_CameraPhi = phimin;
+        // ALTERED
+        if (g_CamType == CAMERA_TYPE_LOOKAT) {
+            // Atualizamos parâmetros da câmera com os deslocamentos
+            g_CameraTheta_lookAt -= 0.01f*dx;
+            g_CameraPhi_lookAt   += 0.01f*dy;
+        
+            // Em coordenadas esféricas, o ângulo phi deve ficar entre -pi/2 e +pi/2.
+            float phimax = 3.141592f/2;
+            float phimin = -phimax;
+        
+            if (g_CameraPhi_lookAt > phimax)
+                g_CameraPhi_lookAt = phimax;
+        
+            if (g_CameraPhi_lookAt < phimin)
+                g_CameraPhi_lookAt = phimin;
+        } else if (g_CamType == CAMERA_TYPE_FREECAM) {
+            // Atualizamos parâmetros da câmera com os deslocamentos
+            g_CameraTheta_freeCam -= 0.01f*dx;
+            g_CameraPhi_freeCam   += 0.01f*dy;
+        
+            // Em coordenadas esféricas, o ângulo phi deve ficar entre -pi/2 e +pi/2.
+            float phimax = 3.141592f/2;
+            float phimin = -phimax;
+        
+            if (g_CameraPhi_freeCam > phimax)
+                g_CameraPhi_freeCam = phimax;
+        
+            if (g_CameraPhi_freeCam < phimin)
+                g_CameraPhi_freeCam = phimin;
+        }
     
         // Atualizamos as variáveis globais para armazenar a posição atual do
         // cursor como sendo a última posição conhecida do cursor.
@@ -1529,7 +1594,7 @@ void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
 {
     // Atualizamos a distância da câmera para a origem utilizando a
     // movimentação da "rodinha", simulando um ZOOM.
-    g_CameraDistance -= 0.1f*yoffset;
+    g_CameraDistance_lookAt -= 0.1f*yoffset;
 
     // Uma câmera look-at nunca pode estar exatamente "em cima" do ponto para
     // onde ela está olhando, pois isto gera problemas de divisão por zero na
@@ -1537,8 +1602,8 @@ void ScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
     // nunca pode ser zero. Versões anteriores deste código possuíam este bug,
     // o qual foi detectado pelo aluno Vinicius Fraga (2017/2).
     const float verysmallnumber = std::numeric_limits<float>::epsilon();
-    if (g_CameraDistance < verysmallnumber)
-        g_CameraDistance = verysmallnumber;
+    if (g_CameraDistance_lookAt < verysmallnumber)
+        g_CameraDistance_lookAt = verysmallnumber;
 }
 
 // Definição da função que será chamada sempre que o usuário pressionar alguma
@@ -1626,11 +1691,47 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mod)
         fflush(stdout);
     }
 
+    //  ALTERED
     // DEBUG apenas: tecla B aumenta score
     if (key == GLFW_KEY_B && action == GLFW_PRESS)
     {
         if (debugMode) g_Score += 500;
     }
+
+    // Player movement
+    // W moves FORWARD
+    if (key == GLFW_KEY_W) {
+        if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+            g_KeyWPressed = true;
+        } else {
+            g_KeyWPressed = false;
+        }
+    }
+    // A moves LEFT
+    if (key == GLFW_KEY_A) {
+        if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+            g_KeyAPressed = true;
+        } else {
+            g_KeyAPressed = false;
+        }
+    }
+    // S moves BACKWARD
+    if (key == GLFW_KEY_S) {
+        if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+            g_KeySPressed = true;
+        } else {
+            g_KeySPressed = false;
+        }
+    }
+    // D moves RIGHT
+    if (key == GLFW_KEY_D) {
+        if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+            g_KeyDPressed = true;
+        } else {
+            g_KeyDPressed = false;
+        }
+    }
+
 }
 
 // Definimos o callback para impressão de erros da GLFW no terminal

21f3fa7cee62e7f4bd6949ea8fea38dc1788e05a
Tiago Ceccon <ceccon.t@hotmail.com>
2021-11-22 11:10:09 -0300

    Change camera look-at point to be the player instead of the origin

--- src/main.cpp
+++ src/main.cpp
@@ -434,7 +434,7 @@ int main(int argc, char* argv[])
     std::srand(time(NULL));
 
     // // Create a few random objects
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 2; i++) {
         GameObject newObject;
         newObject.id = getNextObjectId();
         newObject.pos = glm::vec3(generateRandomSmallFloat()*10, generateRandomSmallFloat()*10, generateRandomSmallFloat()*10);
@@ -534,7 +534,9 @@ int main(int argc, char* argv[])
         // Abaixo definimos as varáveis que efetivamente definem a câmera virtual.
         // Veja slides 195-227 e 229-234 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
         glm::vec4 camera_position_c  = glm::vec4(x,y,z,1.0f); // Ponto "c", centro da câmera
-        glm::vec4 camera_lookat_l    = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
+        // ALTERED
+        // glm::vec4 camera_lookat_l    = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
+        glm::vec4 camera_lookat_l    = glm::vec4(player.pos.x,player.pos.y,player.pos.z,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
         glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
         glm::vec4 camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
 
